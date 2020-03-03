8e032406c91e1c871460f25b661a106500840fb2
sebahaddin285 <58424180+sebahaddin285@users.noreply.github.com>
2020-03-03 20:11:39 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Bilgi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.soruText = new System.Windows.Forms.Label();
+            this.cvp1Btn = new System.Windows.Forms.Button();
+            this.cvp2Btn = new System.Windows.Forms.Button();
+            this.cvp3Btn = new System.Windows.Forms.Button();
+            this.degistir = new System.Windows.Forms.Button();
+            this.scorText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.eskorText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // soruText
+            // 
+            this.soruText.Location = new System.Drawing.Point(40, 40);
+            this.soruText.Name = "soruText";
+            this.soruText.Size = new System.Drawing.Size(609, 71);
+            this.soruText.TabIndex = 0;
+            this.soruText.Text = "label1";
+            // 
+            // cvp1Btn
+            // 
+            this.cvp1Btn.Location = new System.Drawing.Point(76, 139);
+            this.cvp1Btn.Name = "cvp1Btn";
+            this.cvp1Btn.Size = new System.Drawing.Size(75, 23);
+            this.cvp1Btn.TabIndex = 1;
+            this.cvp1Btn.Text = "button1";
+            this.cvp1Btn.UseVisualStyleBackColor = true;
+            this.cvp1Btn.Click += new System.EventHandler(this.cvp1Btn_Click);
+            // 
+            // cvp2Btn
+            // 
+            this.cvp2Btn.Location = new System.Drawing.Point(188, 139);
+            this.cvp2Btn.Name = "cvp2Btn";
+            this.cvp2Btn.Size = new System.Drawing.Size(75, 23);
+            this.cvp2Btn.TabIndex = 2;
+            this.cvp2Btn.Text = "button2";
+            this.cvp2Btn.UseVisualStyleBackColor = true;
+            this.cvp2Btn.Click += new System.EventHandler(this.cvp2Btn_Click);
+            // 
+            // cvp3Btn
+            // 
+            this.cvp3Btn.Location = new System.Drawing.Point(307, 139);
+            this.cvp3Btn.Name = "cvp3Btn";
+            this.cvp3Btn.Size = new System.Drawing.Size(75, 23);
+            this.cvp3Btn.TabIndex = 3;
+            this.cvp3Btn.Text = "button3";
+            this.cvp3Btn.UseVisualStyleBackColor = true;
+            this.cvp3Btn.Click += new System.EventHandler(this.cvp3Btn_Click);
+            // 
+            // degistir
+            // 
+            this.degistir.Location = new System.Drawing.Point(76, 184);
+            this.degistir.Name = "degistir";
+            this.degistir.Size = new System.Drawing.Size(306, 25);
+            this.degistir.TabIndex = 4;
+            this.degistir.Text = "Değiştir";
+            this.degistir.UseVisualStyleBackColor = true;
+            this.degistir.Click += new System.EventHandler(this.degistir_Click);
+            // 
+            // scorText
+            // 
+            this.scorText.AutoSize = true;
+            this.scorText.Location = new System.Drawing.Point(24, 274);
+            this.scorText.Name = "scorText";
+            this.scorText.Size = new System.Drawing.Size(13, 13);
+            this.scorText.TabIndex = 5;
+            this.scorText.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "en yüksek skor:";
+            // 
+            // eskorText
+            // 
+            this.eskorText.AutoSize = true;
+            this.eskorText.Location = new System.Drawing.Point(162, 274);
+            this.eskorText.Name = "eskorText";
+            this.eskorText.Size = new System.Drawing.Size(13, 13);
+            this.eskorText.TabIndex = 7;
+            this.eskorText.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 339);
+            this.Controls.Add(this.eskorText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.scorText);
+            this.Controls.Add(this.degistir);
+            this.Controls.Add(this.cvp3Btn);
+            this.Controls.Add(this.cvp2Btn);
+            this.Controls.Add(this.cvp1Btn);
+            this.Controls.Add(this.soruText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label soruText;
+        private System.Windows.Forms.Button cvp1Btn;
+        private System.Windows.Forms.Button cvp2Btn;
+        private System.Windows.Forms.Button cvp3Btn;
+        private System.Windows.Forms.Button degistir;
+        private System.Windows.Forms.Label scorText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label eskorText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Bilgi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList soruList;
+        ArrayList dogruList;
+        ArrayList yanlis1List;
+        ArrayList yanlis2List;
+
+        Random r;
+        Random r2;
+        int i;
+        int deger;
+        int sayac = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            eskorText.Text = ayarlar.Default.eskor.ToString();
+            r2 = new Random();
+            i = r2.Next(4);
+            degistir.Visible = false;
+            soruList = new ArrayList();
+            dogruList = new ArrayList();
+            yanlis1List = new ArrayList();
+            yanlis2List = new ArrayList();
+            r = new Random();
+            deger = r.Next(2);
+            soruList.Add("türkiyenin başkenti");
+            dogruList.Add("ankara");
+            yanlis2List.Add("tahran");
+            yanlis1List.Add("paris");
+
+            soruList.Add("iranın başkenti");
+            dogruList.Add("tahran");
+            yanlis2List.Add("ankara");
+            yanlis1List.Add("paris");
+
+            soruList.Add("fransa başkenti");
+            dogruList.Add("tahran");
+            yanlis2List.Add("ankara");
+            yanlis1List.Add("paris");
+
+            soruList.Add("amerika başkenti");
+            dogruList.Add("tahran");
+            yanlis2List.Add("ankara");
+            yanlis1List.Add("paris");
+
+            if (deger == 0)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = dogruList[i].ToString();
+                cvp2Btn.Text = yanlis1List[i].ToString();
+                cvp3Btn.Text = yanlis2List[i].ToString();
+            }
+            else if (deger == 1)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = yanlis1List[i].ToString();
+                cvp2Btn.Text = dogruList[i].ToString();
+                cvp3Btn.Text = yanlis2List[i].ToString();
+            }
+            else if (deger == 2)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = yanlis1List[i].ToString();
+                cvp2Btn.Text = yanlis2List[i].ToString();
+                cvp3Btn.Text = dogruList[i].ToString();
+            }
+
+        }
+
+        private void cvp1Btn_Click(object sender, EventArgs e)
+        {
+            if(dogruList[i] == cvp1Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Green;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Red;
+
+                degistir.Visible = true;
+            }
+            else if(dogruList[i] == cvp2Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Green;
+                cvp3Btn.BackColor = Color.Red;
+                cvp2Btn.Enabled = false;
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString(); 
+                }
+            }
+            else if(dogruList[i] == cvp3Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Green;
+                cvp3Btn.Enabled = false;
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString();
+                }
+            }
+
+        }
+
+        private void cvp2Btn_Click(object sender, EventArgs e)
+        {
+            if (dogruList[i] == cvp1Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Green;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Red;
+                cvp1Btn.Enabled = false;
+
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString();
+                }
+            }
+            else if (dogruList[i] == cvp2Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Green;
+                cvp3Btn.BackColor = Color.Red;
+
+                degistir.Visible = true;
+            }
+            else if (dogruList[i] == cvp3Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Green;
+                cvp3Btn.Enabled = false;
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString();
+                }
+            }
+        }
+
+        private void cvp3Btn_Click(object sender, EventArgs e)
+        {
+            if (dogruList[i] == cvp1Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Green;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Red;
+                cvp1Btn.Enabled = false;
+
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString();
+                }
+            }
+            else if (dogruList[i] == cvp2Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Green;
+                cvp3Btn.BackColor = Color.Red;
+                cvp2Btn.Enabled = false;
+                if (sayac > ayarlar.Default.eskor)
+                {
+                    ayarlar.Default.eskor = sayac;
+                    ayarlar.Default.Save();
+                    eskorText.Text = ayarlar.Default.eskor.ToString();
+                }
+            }
+            else if (dogruList[i] == cvp3Btn.Text)
+            {
+                cvp1Btn.BackColor = Color.Red;
+                cvp2Btn.BackColor = Color.Red;
+                cvp3Btn.BackColor = Color.Green;
+
+                degistir.Visible = true;
+            }
+        }
+
+        private void degistir_Click(object sender, EventArgs e)
+        {
+            cvp1Btn.Enabled = true;
+            cvp2Btn.Enabled = true;
+            cvp3Btn.Enabled = true;
+            sayac++;
+            scorText.Text = sayac.ToString();
+            cvp1Btn.BackColor = Color.White;
+            cvp2Btn.BackColor = Color.White;
+            cvp3Btn.BackColor = Color.White;
+            
+            deger = r.Next(3);
+            i = r2.Next(4);
+            if (deger == 0)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = dogruList[i].ToString();
+                cvp2Btn.Text = yanlis1List[i].ToString();
+                cvp3Btn.Text = yanlis2List[i].ToString();
+            }
+            else if (deger == 1)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = yanlis1List[i].ToString();
+                cvp2Btn.Text = dogruList[i].ToString();
+                cvp3Btn.Text = yanlis2List[i].ToString();
+            }
+            else if (deger == 2)
+            {
+                soruText.Text = soruList[i].ToString();
+                cvp1Btn.Text = yanlis1List[i].ToString();
+                cvp2Btn.Text = yanlis2List[i].ToString();
+                cvp3Btn.Text = dogruList[i].ToString();
+            }
+            degistir.Visible = false;
+
+        }
+    }
+}
